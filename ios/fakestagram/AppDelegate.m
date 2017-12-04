6c3c8e70c4a515d9a3ba6e6d80b1bacfd4c02013
David Levai <dlevai94@gmail.com>
2017-12-04 17:08:43 +0100

    linked ios + android

--- ios/fakestagram/AppDelegate.m
+++ ios/fakestagram/AppDelegate.m
@@ -11,11 +11,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
